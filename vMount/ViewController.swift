d437af7cba47e1a9415a336bcbaee2ec874220fe
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-11-19 18:46:18 +0100

    Initial Commit

---
+++ vMount/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  vMount
+//
+//  Created by Dirk Hoffmann on 19.11.25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
