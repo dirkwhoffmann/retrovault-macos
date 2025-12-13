01b831a73a64e48c766a86e7438ca6548f44fd71
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-12-13 10:40:53 +0100

    Added MediaFileProxy

--- ObjCProxy/EmulatorProxy.h
+++ ObjCProxy/EmulatorProxy.h
@@ -10,10 +10,12 @@
 #pragma once
 
 #import "config.h"
+#import "MediaFileTypes.h"
 #import "DeviceTypes.h"
 #import "FSTypes.h"
 #import "FSObjects.h"
 #import "FuseFileSystemTypes.h"
+#import "utl/abilities/Compressible.h"
 #import <Cocoa/Cocoa.h>
 
 using namespace vamiga;
@@ -45,6 +47,48 @@ using namespace vamiga;
 }
 
 - (instancetype) initWith:(void *)ref;
+
+@end
+
+
+//
+// MediaFile
+//
+
+@interface MediaFileProxy : Proxy
+{
+    NSImage *preview;
+}
+
++ (FileType) typeOfUrl:(NSURL *)url;
+
++ (instancetype)make:(void *)file;
++ (instancetype)makeWithFile:(NSString *)path exception:(ExceptionWrapper *)ex;
++ (instancetype)makeWithFile:(NSString *)path type:(FileType)t exception:(ExceptionWrapper *)ex;
++ (instancetype)makeWithBuffer:(const void *)buf length:(NSInteger)len type:(FileType)t exception:(ExceptionWrapper *)ex;
+
+@property (readonly) FileType type;
+@property (readonly) NSInteger size;
+@property (readonly) u64 fnv;
+@property (readonly) Compressor compressor;
+@property (readonly) BOOL compressed;
+
+@property (readonly) u8 *data;
+
+- (void)writeToFile:(NSString *)path exception:(ExceptionWrapper *)ex;
+- (void)writeToFile:(NSString *)path partition:(NSInteger)part exception:(ExceptionWrapper *)ex;
+
+@property (readonly, strong) NSImage *previewImage;
+@property (readonly) time_t timeStamp;
+@property (readonly) DiskInfo diskInfo;
+@property (readonly) HDFInfo hdfInfo;
+@property (readonly) NSString *describeCapacity;
+
+- (NSInteger)readByte:(NSInteger)b offset:(NSInteger)offset;
+- (void)readSector:(NSInteger)b destination:(unsigned char *)buf;
+
+- (NSString *)hexdump:(NSInteger)b offset:(NSInteger)offset len:(NSInteger)len;
+- (NSString *)asciidump:(NSInteger)b offset:(NSInteger)offset len:(NSInteger)len;
 
 @end
 
--- ObjCProxy/EmulatorProxy.mm
+++ ObjCProxy/EmulatorProxy.mm
@@ -59,6 +59,162 @@ using namespace utl;
 
 
 //
+// MediaFile
+//
+
+@implementation MediaFileProxy
+
+- (MediaFile *)file
+{
+    return (MediaFile *)obj;
+}
+
++ (instancetype)make:(void *)file
+{
+    return file ? [[self alloc] initWith:file] : nil;
+}
+
++ (FileType)typeOfUrl:(NSURL *)url
+{
+    return MediaFile::type([url fileSystemRepresentation]);
+}
+
++ (instancetype)makeWithFile:(NSString *)path
+                   exception:(ExceptionWrapper *)ex
+{
+    try { return [self make: MediaFile::make([path fileSystemRepresentation])]; }
+    catch(Error &error) { [ex save:error]; return nil; }
+}
+
++ (instancetype)makeWithFile:(NSString *)path
+                        type:(FileType)type
+                   exception:(ExceptionWrapper *)ex
+{
+    try { return [self make: MediaFile::make([path fileSystemRepresentation], type)]; }
+    catch(Error &error) { [ex save:error]; return nil; }
+}
+
++ (instancetype)makeWithBuffer:(const void *)buf length:(NSInteger)len
+                          type:(FileType)type
+                     exception:(ExceptionWrapper *)ex
+{
+    try { return [self make: MediaFile::make((u8 *)buf, len, type)]; }
+    catch(Error &error) { [ex save:error]; return nil; }
+}
+
+- (FileType)type
+{
+    return [self file]->type();
+}
+
+- (u64)fnv
+{
+    return [self file]->fnv64();
+}
+
+- (NSInteger)size
+{
+    return [self file]->getSize();
+}
+
+- (Compressor)compressor
+{
+    return [self file]->compressor();
+}
+
+- (BOOL)compressed
+{
+    return [self file]->isCompressed();
+}
+
+- (u8 *)data
+{
+    return [self file]->getData();
+}
+
+- (void)writeToFile:(NSString *)path exception:(ExceptionWrapper *)ex
+{
+    try { [self file]->writeToFile(string([path fileSystemRepresentation])); }
+    catch(Error &err) { [ex save:err]; }
+}
+
+- (void)writeToFile:(NSString *)path partition:(NSInteger)part exception:(ExceptionWrapper *)ex
+{
+    try { [self file]->writePartitionToFile(string([path fileSystemRepresentation]), part); }
+    catch(Error &err) { [ex save:err]; }
+}
+
+- (NSImage *)previewImage
+{
+    // Return cached image (if any)
+    if (preview) { return preview; }
+
+    // Get dimensions and data
+    auto size = [self file]->previewImageSize();
+    auto data = (unsigned char *)[self file]->previewImageData();
+
+    // Create preview image
+    if (data) {
+
+        NSBitmapImageRep *rep = [[NSBitmapImageRep alloc]
+                                 initWithBitmapDataPlanes: &data
+                                 pixelsWide:size.first
+                                 pixelsHigh:size.second
+                                 bitsPerSample:8
+                                 samplesPerPixel:4
+                                 hasAlpha:true
+                                 isPlanar:false
+                                 colorSpaceName:NSCalibratedRGBColorSpace
+                                 bytesPerRow:4*size.first
+                                 bitsPerPixel:32];
+
+        preview = [[NSImage alloc] initWithSize:[rep size]];
+        [preview addRepresentation:rep];
+
+        // image.makeGlossy()
+    }
+    return preview;
+}
+
+- (time_t)timeStamp
+{
+    return [self file]->timestamp();
+}
+
+- (DiskInfo)diskInfo
+{
+    return [self file]->getDiskInfo();
+}
+
+-(HDFInfo)hdfInfo
+{
+    return [self file]->getHDFInfo();
+}
+
+- (NSInteger)readByte:(NSInteger)b offset:(NSInteger)offset
+{
+    return [self file]->readByte(b, offset);
+}
+
+- (void)readSector:(NSInteger)b destination:(unsigned char *)buf
+{
+    [self file]->readSector(buf, b);
+}
+
+- (NSString *)hexdump:(NSInteger)b offset:(NSInteger)offset len:(NSInteger)len
+{
+    return @([self file]->hexdump(b, offset, len).c_str());
+}
+
+- (NSString *)asciidump:(NSInteger)b offset:(NSInteger)offset len:(NSInteger)len
+{
+    return @([self file]->asciidump(b, offset, len).c_str());
+}
+
+@end
+
+
+//
 // AmigaDeviceProxy
 //
 
